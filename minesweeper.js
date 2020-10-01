fcff81749542a3041f49e1bef91be3a0eced0c59
Robert_Hamilton <robert_hamilton36@hotmail.com>
2020-10-01 18:43:44 +1300

    add function can't die on first turn

--- minesweeper.js
+++ minesweeper.js
@@ -1,227 +1,161 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-// Define your `board` object here!
 
-var board = {}
+var board = {
+}
 
 let boardSize = 4
-let numberOfMines = (boardSize*boardSize/3)
-
-
-
-
-// var board = {
-//   cells : [
-//     {row:0, col:0, isMine:true, hidden: true}, 
-//     {row:0, col:1, isMine:false, hidden: true}, 
-//     {row:0, col:2, isMine:true, hidden: true},
-//     {row:0, col:3, isMine:true, hidden: true},
-//     {row:1, col:0, isMine:false, hidden: true},
-//     {row:1, col:1, isMine:true, hidden: true}, 
-//     {row:1, col:2, isMine:false, hidden: true},
-//     {row:1, col:3, isMine:false, hidden: true},
-//     {row:2, col:0, isMine:true, hidden: true},
-//     {row:2, col:1, isMine:true, hidden: true}, 
-//     {row:2, col:2, isMine:false, hidden: true},
-//     {row:2, col:3, isMine:true, hidden: true},
-//     {row:3, col:0, isMine:true, hidden: true},
-//     {row:3, col:1, isMine:true, hidden: true}, 
-//     {row:3, col:2, isMine:false, hidden: true},
-//     {row:3, col:3, isMine:true, hidden: true}  
-//   ]
-// }
+let numberOfBombs = (boardSize*boardSize/3)
 
 
 function startGame () {
-  board = createBoard(boardSize,numberOfMines)
-  createMineOptions()
-  document.addEventListener ("click", checkForWin)
-  document.addEventListener ("contextmenu", checkForWin)
-  // document.addEventListener ("submit", howManyMines(this.value))
+  board = createBoard(boardSize, numberOfBombs)
+  document.addEventListener("click", checkForWin)
+  document.addEventListener("contextmenu", checkForWin)
 
-  for (i in board.cells){
-    board.cells[i].surroundingMines = countSurroundingMines (board.cells[i])
+  for(var i of board.cells){
+    i.surroundingMines = countSurroundingMines(i)
   }
-
-
+ 
   lib.initBoard()
-
-  console.log(board)
 }
 
+// Function to check win conditions
+// Used in startGame eventlisteners
 
 function checkForWin () {
-  for (i = 0; i< board.cells.length; i++){ 
-    if(board.cells[i].isMine && board.cells[i].isMarked){
+  for(x of board.cells){
+    if(x.isMine && x.isMarked){
       continue
-    }
-    else if(board.cells[i].isMine && !board.cells[i].isMarked){ 
+    }else if(x.isMine && !x.isMarked){
       return
-    }
-    else if(board.cells[i].hidden){
+    }else if(x.hidden){
       return
     }
   }
-
-  console.log("all marked")
   lib.displayMessage('You win!')
 }
 
-
+//function that counts number of cells that are mines around everycell in game. Called in StartGame()
 function countSurroundingMines (cell) {
-  var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-  let count = 0
-  for (i = 0; i < surrounding.length; i++){
-    if (surrounding[i].isMine){
-      count +=1
-    
+  var surroundingCells = getSurroundingCells(cell.row, cell.col);
+  var count = 0
+  for(x of surroundingCells){
+    if(x.isMine){
+      count+=1
     }
   }
   return count
-
 }
 
 
-// function createBoard (value = 4, bombs = (value*value/3)){
-function createBoard (value , bombs){
-  clearBoard()
+//function called when a different sized board is made
+function clearBoard(){
+  document.querySelector(".board").innerHTML =""
+}
+//Function takes in size of board, and number of bombs
+//Returns a randomized array of true (for number of bombs) and false (for leftovers)
+//To be called only in createBoard
+function makeBombArray(size, bombs){
+  bombsArray = []
 
-  value = value || boardSize
-  bombs = bombs || (value*value/3)
+  //add true for number of bombs
+  for(i = 0; i < bombs; i++){
+    bombsArray.push(true)
+  }
+
+  //add false for number of cells that are not bombs
+  for(i = 0; i < (size*size)-bombs; i++){
+    bombsArray.push(false)
+  }
+  bombsArray = (bombsArray.sort(() => Math.random() - 0.5))
+  return bombsArray
+}
+
+//Function that takes in size of board and number of bombs
+//Returns a randomized board with corresponding arguments
+function createBoard (size, bombs){
+  clearBoard()
+  bombsArray = makeBombArray(size,bombs)
 
   let board = {
     cells:[]
   }
-  // for(i = 0; i < (value * value); i++){
-  //   board.cells.push({})
-  // }
   let t = 0
-  for (i = 0; i < value; i++){
-    for (j = 0; j < value; j++){
-      board.cells[t]={
-        row:i, col:j, isMine:false, hidden: true
-      }
+  for(i = 0; i < size; i++){
+    for(j = 0; j < size; j++){
+      board.cells[t]= {row:i, col:j, isMine:bombsArray[t], hidden: true}
       t++
-
     }
   }
-  board = addBombs((value*value),bombs, board)
-
   return board
 }
 
-function addBombs(cells, bombs, board){
-  let t = []
-  for(i=0; i < bombs; i++){
-    t.push(true)
-  }
-  for(i=0; i< (cells-bombs); i++){
-    t.push(false)
-  }
-  // shuffle(t)
 
-  t = (t.sort(() => Math.random() - 0.5))
-
-  for(i=0; i<cells; i++){
-    board.cells[i].isMine = t[i]
-  }
-
- return board
+//Function that takes in a value and makes the main variable boardSize that variable, restarts the game
+function changeBoardSize(size){
+  boardSize = size
+  numberOfBombs = (boardSize*boardSize/3)
+  createMineOptions()
+  startGame()
 }
 
 
-function clearBoard(){
-  document.querySelector(".board").innerHTML =""
-}
-
-
-function changeBoardSize(value){
-  console.log(boardSize)
-  boardSize = value
-  console.log(boardSize)
-  board = createBoard(value)
-  console.log(value)
-  for (i in board.cells){
-    board.cells[i].surroundingMines = countSurroundingMines (board.cells[i])
-  }
-  createMineOptions(value)
-  lib.initBoard()
-}
-
-function restart(){
-  board=createBoard(boardSize, numberOfMines)
-  for (i in board.cells){
-    board.cells[i].surroundingMines = countSurroundingMines (board.cells[i])
-  }
-  lib.initBoard()
-  return 
-}
-
-
-function checkAllHidden(board){
-  for(i=0 ; i < board.cells.length; i++){
-    if (!board.cells[i].hidden){
-      return false
-    }
-    return true
-  } 
-}
-
+//Function that is used when the checkbox to manually choose number of bombs is clicked.
 function chooseMine(){
-  difficulty((boardSize*boardSize/3))
-  var checkBox = document.getElementById("mineChoice")
+  //changes numberOfBombs to default, for when you uncheck box
+  numberOfBombs=(boardSize*boardSize/3)
+  startGame()
 
+  var checkBox = document.getElementById("mineChoice")
   var mineEntry = document.getElementById("mineEntryField")
 
+//changes the style of the dropdown box to visible or hidden
   if(checkBox.checked == true){
     mineEntry.style.display = "inline-block";
+    createMineOptions()
   } else {
     mineEntry.style.display = "none";
   }
 
 }
 
-function howManyMines(value){
-  console.log("this worked")
-  numberOfMines = value
-  console.log(numberOfMines)
-  restart()
-  board = createBoard(boardSize, numberOfMines)
-  lib.initBoard()
-}
 
-
+//function called to make options for number of bombs equal to number of cells. called when size is changed, or option to choose bombs is choosen
 function createMineOptions(value){
-
-  console.log("changing" + value)
+  value = value || numberOfBombs
   let element = document.getElementById("mineValue");
+
+  //Removes previous options
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
+
+  //creates new options for current board size
   for(var i = 0; i < (boardSize*boardSize); i++){
     var opt = document.createElement('option');
-    if(i+1 == value){
+    //Makes default selected equal to current Number of bombs
+    if(i+1 == Math.floor(numberOfBombs)){
       opt.selected = "selected"
     }
     opt.value = i+1;
     opt.innerHTML = i+1;
     document.getElementById('mineValue').appendChild(opt);
   }
-
-
 }
 
-function difficulty(value){
-  console.log(value)
-  numberOfMines = value
-  console.log
-  clearBoard()
-  board = createBoard(boardSize, numberOfMines)
-  for (i in board.cells){
-    board.cells[i].surroundingMines = countSurroundingMines (board.cells[i])
-  }
-  createMineOptions(value)
-  lib.initBoard()
-  console.log(numberOfMines+" mines")
+//function called by bomb dropdown menu onchange
+function changeNumberOfBombs(value){
+  numberOfBombs = value
+  startGame()
+}
 
+function checkAllIsHidden(){
+  for(x of board.cells){
+    if(!x.hidden){
+      return false
+    }
+  }
+
+return true
 }
